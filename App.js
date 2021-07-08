e0814c4abb291bcaf4ea4a1684732523a64f9f18
abhi98041 <abhi98041@gmail.com>
2021-07-08 08:54:30 +0530

    added floating button and DB

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FloatingAction } from "react-native-floating-action";
+import * as SQLite from 'expo-sqlite';
+
+const db=SQLite.openDatabase(
+  {
+    name: 'MeterTread',
+    location: 'default',
+  },()=>{},
+  error =>{console.log(error)}
+)
 
 export default function App() {
+  const actions = [
+    {
+      text: "Add New Meter",
+      icon: require("./assets/icons/icons8-energy-meter-96.png"),
+      name: "bt_meter",
+      position: 1
+    }
+  ];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<View style={styles.container}>
+  <Text style={styles.example}>Floating Action example</Text>
+  <FloatingAction
+    actions={actions}
+    onPressItem={name => {
+      console.log(`selected button: ${name}`);
+    }}
+  />
+</View>
   );
 }
 
